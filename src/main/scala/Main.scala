332291b0f09ea5b929be10eda9eb1c6dd47af09f
Edward A Maxedon, Sr <edadma@gmail.com>
2019-08-23 10:15:03 -0400

    start coding player

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -12,10 +12,10 @@ object Main extends App {
 
   val fund = 220
   val h1 = Oscillator.sinWave( fund )
-  val h2 = Shape.amplitude( Oscillator.sinWave(fund*3), 1.0/3 )
-  val h3 = Shape.amplitude( Oscillator.sinWave(fund*5), 1.0/5 )
-  val h4 = Shape.amplitude( Oscillator.sinWave(fund*7), 1.0/7 )
-  val h5 = Shape.amplitude( Oscillator.sinWave(fund*9), 1.0/9 )
+  val h2 = Shape.amplitude( Oscillator.sinWave(fund*3), 1/3d )
+  val h3 = Shape.amplitude( Oscillator.sinWave(fund*5), 1/5d )
+  val h4 = Shape.amplitude( Oscillator.sinWave(fund*7), 1/7d )
+  val h5 = Shape.amplitude( Oscillator.sinWave(fund*9), 1/9d )
   val src = Shape.length( h1, 1 ) concat
     Shape.length( Mixer( List(h1, h2) ), 1 ) concat
     Shape.length( Mixer( List(h1, h2, h3) ), 1 ) concat
@@ -31,7 +31,8 @@ object Main extends App {
 //      system.terminate
 //  }
 
-  Output.toMonoWaveFile( src, Paths.get("tone.wav") )
+  Player( Shape.length(h1, 1) )
+//  Output.toMonoWaveFile( src, Paths.get("tone.wav") )
 //  Scope( src )
   system.terminate
 
---
+++ src/main/scala/Player.scala
@@ -0,0 +1,46 @@
+package xyz.hyperreal.symfonia
+
+import akka.stream.scaladsl.{Sink, Source}
+import javax.sound.sampled.{AudioFormat, AudioSystem}
+
+import scala.concurrent.Await
+
+
+object Player {
+
+  def apply( src: Source[Double, _] ) = {
+
+    val thread =
+      new Thread {
+        override def run = {
+          val format = new AudioFormat( Symfonia.sps, 16, 1, true, true )
+          val line = AudioSystem.getSourceDataLine( format )
+          val blocks = Output.toMonoPCMBytes( src ) grouped line.getBufferSize
+
+          line.open( format )
+          line.start
+
+          blocks runForeach {
+            block =>
+              val array = block.toArray
+
+              line.write( array, 0, array.length )
+          }
+
+          line.drain
+          line.stop
+        }
+      }
+
+    thread.start
+    thread.join
+
+  }
+
+}
+
+trait PlayerControl {
+
+
+
+}
